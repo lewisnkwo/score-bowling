a87e086ba1bb522349798258f7c765e3b98f2abc
Lewis Nkwo <lewisnkwo@live.co.uk>
2020-12-21 17:17:41 +0000

    Initial commit, added roll, getScores, createFrame, score

---
+++ bowling.ts
@@ -0,0 +1,78 @@
+type FrameCase = "open" | "spare" | "strike";
+
+interface Roll {
+  pins: number; // 'pins' as in the number of pins knocked down
+  pinsLeft: number | undefined;
+  strike: boolean;
+}
+
+interface Frame {
+  scores: number[];
+  case: FrameCase;
+}
+
+class Bowling {
+  constructor(public rolls: number[]) {}
+
+  roll = (pins: number): Roll => {
+    return {
+      pins,
+      pinsLeft: pins < 10 ? 10 - pins : undefined,
+      strike: pins === 10,
+    };
+  };
+
+  getScores = (firstRoll: Roll, secondRoll: Roll, thirdRoll: Roll): Frame => {
+    const scores: number[] = [];
+
+    if (firstRoll.strike) {
+      // continue...
+    } else if (firstRoll.pins + secondRoll.pins === 10) {
+      scores.push(10);
+      scores.push(thirdRoll.pins);
+      return {
+        scores,
+        case: "spare",
+      };
+    } else {
+      firstRoll.pins;
+      scores.push(10);
+      return {
+        scores,
+        case: "open",
+      };
+    }
+  };
+
+  createFrame = (): Frame => {
+    const randomPins = (pinLimit?: number) =>
+      pinLimit
+        ? Math.floor(Math.random() * (pinLimit + 1))
+        : Math.floor(Math.random() * 11);
+
+    const roll = (pins: number) => this.roll(pins);
+
+    const firstRoll = () => roll(randomPins());
+
+    const secondRoll = (firstRoll: Roll) =>
+      !firstRoll.strike
+        ? roll(randomPins(firstRoll.pinsLeft))
+        : roll(randomPins());
+
+    const thirdRoll = () => roll(randomPins());
+
+    const scores = this.getScores(
+      firstRoll(),
+      secondRoll(firstRoll()),
+      thirdRoll()
+    );
+
+    return scores;
+  };
+
+  score = (): number => {
+    return this.rolls.reduce((first, second) => first + second, 0);
+  };
+}
+
+export default Bowling;
